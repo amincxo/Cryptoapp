38391a6c4c252871f233eb60675e346c4ba9f325
unknown <amincxo@gmail.com>
2024-09-07 18:25:22 +0330

    styles for tableCoin complate

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -4,12 +4,14 @@ import { RotatingLines } from 'react-loader-spinner'
 import chartUp from "../../assets/chart-up.svg"
 import chartDown from "../../assets/chart-down.svg"
 
+import styles from './TableCoin.module.css'
+
 function TableCoin({coins , isLoding}) {
   return (
-    <div>
+    <div className={styles.container} >
     {isLoding ? (<RotatingLines strokeColor='#3874ff' strokeWidth='2'  />) :
         (
-        <table>
+        <table className={styles.table} >
             <thead>
                 <tr>
                     <th>نماد سکه</th>
@@ -48,14 +50,14 @@ const TableRow = ({
     return (
         <tr>
         <td>
-            <div>
+            <div className={styles.symbol} >
                 <img src={image} alt={symbol} />
                 <span> {symbol.toUpperCase()} </span>
             </div>
         </td>
         <td>{name}</td>
         <td>${current_price.toLocaleString()}</td>
-        <td>{price_change.toFixed(2)}%</td>
+        <td className={price_change > 0 ? styles.success : styles.error} >{price_change.toFixed(2)}%</td>
         <td>{total_volume.toLocaleString()}</td>
         <td>
             <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
