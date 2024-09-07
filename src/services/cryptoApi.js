c4c7b90b9975de1366c8748b2076a6e64ad264f5
unknown <amincxo@gmail.com>
2024-09-07 11:02:58 +0330

    remove api

--- src/services/cryptoApi.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL = "https://api.coingecko.com/api/v3";
-const API_KEY = "CG-TJcWZyC8esmJ2byAVXXRM1dr";
-
-const getCoinList = () => {
-    return `${BASE_URL}/coins/markets?vs_currency=usd&per_page=20&page=1&x-cg-demo-api-key=${API_KEY}`
-}
-
-export {getCoinList};
